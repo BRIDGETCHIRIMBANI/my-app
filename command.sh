d20cf88b19e842660042ae66d87d160d1e8a9218
BRIDGETCHIRIMBANI <bridgetchirimbani22@gmail.com>
2022-10-05 22:57:11 +0400

    create command.sh

---
+++ command.sh
@@ -0,0 +1,23 @@
+# 1
+npx create-react-app my-app
+cd my-app
+npm start
+
+#2
+git init 
+git add .
+git status
+git commit -m "modify project files"
+
+#3
+git branch -M update_logo
+
+#6
+git add
+git commit
+git push --set-upstream origin update_logo
+
+
+
+
+
